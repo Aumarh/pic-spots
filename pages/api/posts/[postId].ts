b8f4de2e71d464f7aa2a91ebac15922f0d7fd2a5
Aumarh <aumarh@riseup.net>
2022-07-22 14:34:43 +0200

    Add post API error patch

--- pages/api/posts/[postId].ts
+++ pages/api/posts/[postId].ts
@@ -23,13 +23,13 @@ export default async function postHandler(
   if (request.method === 'DELETE') {
     const deletedPost = await deletePostByPostId(request.body.postId);
     console.log('this is deleted post', deletedPost);
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (!deletedPost) {
-      response.status(404).json({ errors: 'Post not found' });
-      return;
-    }
 
-    response.status(200).json({ post: deletedPost });
+    // if (!deletedPost) {
+    //   response.status(404).json({ errors: 'Post not found' });
+    //   return;
+    // }
+
+    response.status(200).json({ post: deletedPost as any });
     return;
   }
 
